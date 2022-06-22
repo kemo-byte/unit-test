1409fb88b03fa27e0151fd0534ecc70ea49760d1
kemobyte <kamalkafi12@gmail.com>
2022-06-22 13:25:54 +0200

    addition operation
    Addition class and  Operation Interfce

--- tests/unit/Calculator/AdditionTest.php
+++ tests/unit/Calculator/AdditionTest.php
@@ -4,4 +4,16 @@
 class AdditionTest extends \PHPUnit\Framework\TestCase
 {
 
+    /** @test */
+
+    public function adds_up_given_operands()
+    {
+        $addition = new \App\Calculator\Addition;
+
+        $addition->setOperands([5,10]);
+
+        $this->assertEquals(15, $addition->calculate());
+    }
+
+    
 }
--- tests/unit/SampleTest.php
+++ tests/unit/SampleTest.php
@@ -3,4 +3,8 @@
 // class SampleTest extends \PHPUnit_Framework_TestCase
 class SampleTest extends \PHPUnit\Framework\TestCase
 {
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
 }
