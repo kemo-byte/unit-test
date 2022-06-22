e6c6ae89c6686374f393425de0c35ab3657957a2
kemobyte <kamalkafi12@gmail.com>
2022-06-22 15:08:34 +0200

    Division refactor with array_filter and array_reduce

--- app/Calculator/Division.php
+++ app/Calculator/Division.php
@@ -10,21 +10,16 @@ class Division extends OperationAbstract implements OperationInterface
 
     public function calculate()
     {
-        if(count($this->operands) === 0) {
+        if (count($this->operands) === 0) {
             throw new NoOperandsException;
         }
 
-        $result = 0;
-
-        foreach ($this->operands as $index => $operand) {
-            if($index === 0) {
-                $result = $operand;
-                continue;
+        return array_reduce(array_filter($this->operands), function ($a, $b) {
+            if ($a !== null && $b !== null) {
+                return $a / $b;
             }
-        
-            $result /= $operand;
-        }
-        return $result;
 
+            return $b;
+        }, null);
     }
 }
